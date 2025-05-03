4b9f5feb333737eb28b917b8c8fc8dd71256f327
usrofgh <test@gmail.com>
2025-05-03 16:13:31 +0300

    Change genres model structure

--- src/domains/genres/models.py
+++ src/domains/genres/models.py
@@ -1,10 +1,4 @@
-from datetime import datetime
-from uuid import uuid4
-
-from sqlalchemy import func
-from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy.types import DateTime
 
 from src.core.base_model import BaseModel
 from src.domains.associations.production_genre import production_genre_associations
@@ -12,12 +6,7 @@ from src.domains.associations.production_genre import production_genre_associati
 
 class GenreModel(BaseModel):
     __tablename__ = "genres"
-
-    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     name: Mapped[str] = mapped_column(unique=True, index=True)
-    description: Mapped[str]
-    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
-    updated_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), onupdate=func.now())
 
     productions: Mapped[list["ProductionModel"]] = relationship(
         back_populates="genres",
